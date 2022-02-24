5f70a3c70659469710fc513583c097061be7883d
Nyyir <nyyirs@gmail.com>
2022-02-24 12:54:31 +0400

    working Category on client

--- BlazorEcommerce/Server/Services/ProductService/ProductService.cs
+++ BlazorEcommerce/Server/Services/ProductService/ProductService.cs
@@ -26,6 +26,20 @@
             return responce;
         }
 
+        public async Task<ServiceResponse<List<Product>>> GetProductByCategory(string categoryUrl)
+        {
+            var response = new ServiceResponse<List<Product>>
+            {
+
+                Data = await _context.Products
+                .Where(p => p.Category.Url.ToLower().Equals(categoryUrl.ToLower()))
+                .ToListAsync()
+
+            };
+
+            return response;
+        }
+
         public async Task<ServiceResponse<List<Product>>> GetProductsAsync() {
 
             var response = new ServiceResponse<List<Product>>
